c9cb18e45a6d6e0f0830a2b154652345d3b68299
qc <58107143+Luoji-0@users.noreply.github.com>
2024-12-16 22:33:21 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -180,7 +180,8 @@ const USER_CONFIG = {
       name: '晗宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       // 使用其他通道时，请严格按照各个通道的教程进行填写
-      id: 'ofxzP6LAKqwxDAsImmpFMJBflXDM',
+      // id: 'ofxzP6LAKqwxDAsImmpFMJBflXDM',
+      id: 'ofxzP6OZemxxgdem0pUAiRw8dwLs',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
       useTemplateId: 'XGP6vPwdpPDEv5sZ7GEwD0cUSSaXqTr0YayP5F8tLOM',
